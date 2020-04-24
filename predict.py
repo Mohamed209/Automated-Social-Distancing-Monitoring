275e3aeee74d6e4a316c7b806fa582d92a52eeb0
mohamed209 <mohamedmosad209@gmail.com>
2020-04-24 22:32:03 +0200

    :sparkles: added kmeans

--- predict.py
+++ predict.py
@@ -1,9 +1,11 @@
 import torch
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 from model.model import CSRNet
 from torchvision import datasets, transforms
 from matplotlib import cm as c
+from sklearn.cluster import KMeans
 from PIL import Image
 model = CSRNet()
 # loading the trained weights
@@ -16,12 +18,23 @@ transform = transforms.Compose([
 ])
 # check preds for single image
 img = transform(Image.open(
-    'test-data/images/960x0.jpg').convert('RGB'))
+    'test-data/images/3.png').convert('RGB'))
 output = model(img.unsqueeze(0))
-print("Predicted Count : ", int(output.detach().cpu().sum().numpy()))
+
+count = int(output.detach().cpu().sum().numpy())
+print("Predicted Count : ", count)
 temp = np.asarray(output.detach().cpu().reshape(
-    output.detach().cpu().shape[2], output.detach().cpu().shape[3]))
+    output.detach().cpu().shape[2], output.detach().cpu().shape[3]), dtype=np.float)*255
 print("density matrix \n", temp)
+# temp = cv2.GaussianBlur(temp, (3, 3), 0)
 plt.imshow(temp, cmap=c.jet)
+plt.colorbar()
 plt.show()
-# TODO : discretize the density map
+# cluster points
+kmeans = KMeans(n_clusters=count, n_jobs=-1)
+kmeans.fit(temp.reshape(-1, 2))
+print(kmeans.cluster_centers_)
+print(kmeans.cluster_centers_.shape)
+plt.scatter(x=kmeans.cluster_centers_[:, 0], y=kmeans.cluster_centers_[:, 1])
+plt.show()
+# # TODO : discretize the density map
