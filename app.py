5924c37e3606ecbb035fd295249bccef8636af36
mohamed209 <mohamedmosad209@gmail.com>
2020-06-07 15:21:15 +0200

    :construction: added basic dashboard

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from src.object_detector.yolov3 import YoloPeopleDetector
 from src.object_detector.postprocessor import PostProcessor
 from src.visualization.visualizer import CameraViz
 import cv2
+import dash_bootstrap_components as dbc
 
 
 def stream_test_local_video(path):
@@ -16,11 +17,11 @@ def stream_test_local_video(path):
         # Capture frame-by-frame
         ret, frame = cap.read()
         if ret == True:
-            # outs = net.predict(frame)
-            # pp = PostProcessor()
-            # indices, boxes, ids, confs, centers = pp.process_preds(frame, outs)
-            # cameraviz = CameraViz(indices, frame, ids, confs, boxes, centers)
-            # cameraviz.draw_pred()
+            outs = net.predict(frame)
+            pp = PostProcessor()
+            indices, boxes, ids, confs, centers = pp.process_preds(frame, outs)
+            cameraviz = CameraViz(indices, frame, ids, confs, boxes, centers)
+            cameraviz.draw_pred()
             frame = cv2.imencode('.jpg', frame)[1].tobytes()
             yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
         else:
@@ -28,7 +29,8 @@ def stream_test_local_video(path):
 
 
 server = Flask(__name__)
-app = dash.Dash(__name__, server=server)
+app = dash.Dash(__name__, server=server,
+                external_stylesheets=[dbc.themes.SUPERHERO])
 
 
 @server.route('/video_feed')
@@ -36,13 +38,31 @@ def video_feed():
     return Response(stream_test_local_video(path='data/test_videos/cut3.mp4'), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-app.layout = html.Div([
-    html.H1("Real Time Social Distancing Monitor", id='page_header',),
-    html.Div(html.Img(src="/video_feed", height=500, width=800))
-])
+app.layout = dbc.Container(
+    [
+        html.H1("Real Time Social Distancing Monitor",
+                style={'text-align': 'center'}),
+        dbc.Row(
+            [
+                dbc.Col(html.Div(id='video_stream', children=[html.Img(
+                    src="/video_feed", height=400, width=800)]), md=4.5),
+                dbc.Col(dcc.Graph(id="phones-graph"))
+            ],
+            align="start"
+        ),
+        dbc.Row(
+            [
+                dbc.Col(dcc.Graph(id="phones-graphh")),
+                dbc.Col(dcc.Graph(id="phones-graphhh"))
+            ],
+            align="start",
+        )
+    ],
+    fluid=True,
+)
 
 if __name__ == '__main__':
     # init yolo network , postprocessor and visualization mode
-    # net = YoloPeopleDetector()
-    # net.load_network()
+    net = YoloPeopleDetector()
+    net.load_network()
     app.run_server(debug=True)
