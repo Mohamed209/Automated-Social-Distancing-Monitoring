29ef68faa2e333d0c1bf667901db2e1c7ca89422
mohamed209 <mohamedmosad209@gmail.com>
2020-06-09 21:51:03 +0200

    :construction: linked app to sqlalchemy db

--- app.py
+++ app.py
@@ -7,12 +7,29 @@ import cv2
 import dash_bootstrap_components as dbc
 import datetime
 import numpy as np
+import os
 from flask import Flask, Response
+from flask_sqlalchemy import SQLAlchemy
 from src.object_detector.yolov3 import YoloPeopleDetector
 from src.object_detector.postprocessor import PostProcessor
 from src.visualization.visualizer import CameraViz
 from src.data_feed.data_feeder import ViolationsFeed
 from dash.dependencies import Input, Output, State
+
+server = Flask(__name__)
+server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+server.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+db = SQLAlchemy(server)
+app = dash.Dash(__name__, server=server,
+                external_stylesheets=[dbc.themes.SUPERHERO])
+
+
+# database
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    location = db.Column(db.String(50))
+    date_created = db.Column(db.DateTime, default=datetime.datetime.now)
 
 
 def stream_test_local_video(path):
@@ -34,12 +51,12 @@ def stream_test_local_video(path):
         else:
             break
 
-
-server = Flask(__name__)
-app = dash.Dash(__name__, server=server,
-                external_stylesheets=[dbc.themes.SUPERHERO])
-
-
+# test db
+user = User(name='mossad', location='egypt')
+db.session.add(user)
+db.session.commit()
+data = User.query.all()
+print(data[0].name)
 @server.route('/video_feed')
 def video_feed():
     return Response(stream_test_local_video(path='data/test_videos/cut3.mp4'), mimetype='multipart/x-mixed-replace; boundary=frame')
@@ -80,12 +97,10 @@ def update_violations_graph(n):
     # data collection
     vio, nonvio = vf.get_feed()
     t = datetime.datetime.now()
-    fig.add_trace(go.Bar(name='Violations', x=[t], y=vio))
-    fig.add_trace(go.Bar(name='Non Violations', x=[t], y=nonvio))
-    # fig = go.Figure(data=[
-    #     go.Bar(name='Violations', x=[t], y=vio),
-    #     go.Bar(name='Non Violations', x=[t], y=nonvio)
-    # ])
+    fig = go.Figure(data=[
+        go.Bar(name='Violations', x=[t], y=vio),
+        go.Bar(name='Non Violations', x=[t], y=nonvio)
+    ])
     vf.clear_feed()
     return fig
 
@@ -93,9 +108,12 @@ def update_violations_graph(n):
 if __name__ == '__main__':
     # init data feed pipes
     vf = ViolationsFeed()
+    ## init vio graph ##
     fig = go.Figure()
     fig.update_layout(
         barmode='stack', title_text='Violations VS Non Violations Graph')
+    ## vio graph ##
+
     # init and load yolo network
     net = YoloPeopleDetector()
     net.load_network()
