63b12299703995899eeed1ab318ce2604a2ed0e9
mohamed209 <mohamedmosad209@gmail.com>
2020-06-15 23:07:31 +0200

    :rocket: semifinal version

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ from src.object_detector.postprocessor import PostProcessor
 from src.visualization.visualizer import CameraViz
 from src.data_feed.data_feeder import ViolationsFeed
 from dash.dependencies import Input, Output, State
-
+from PIL import Image
+from heatmappy import Heatmapper
 server = Flask(__name__)
 server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 server.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
@@ -35,6 +36,13 @@ class Violations(db.Model):
 class Sevidx(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sev = db.Column(db.Float, nullable=False)
+    time = db.Column(db.DateTime, default=datetime.now())
+
+
+class Vioxy(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    violocx = db.Column(db.Float, nullable=False)
+    violocy = db.Column(db.Float, nullable=False)
     time = db.Column(db.DateTime, default=datetime.now())
 
 
@@ -70,8 +78,8 @@ app.layout = html.Div([dbc.Container(
                 style={'text-align': 'center'}),
         dbc.Row(
             [
-                dbc.Col(html.Div(id='video_stream', children=[html.Img(
-                    src="/video_feed", height=400, width=800)]), md=4.5),
+                dbc.Col(html.Img(
+                    src="/video_feed", height=400, width=800), md=4.5),
                 dbc.Col(dcc.Graph(id="violations"))
             ],
             align="start"
@@ -79,7 +87,7 @@ app.layout = html.Div([dbc.Container(
         dbc.Row(
             [
                 dbc.Col(dcc.Graph(id="sev-idx")),
-                dbc.Col(dcc.Graph(id="phones-graphhh"))
+                dbc.Col(dcc.Graph(id="heat-map"))
             ],
             align="start",
         )
@@ -98,10 +106,8 @@ app.layout = html.Div([dbc.Container(
 def update_violations_graph(n):
     # plotting variables
     t, violist, nonviolist = [], [], []
-    vio, nonvio, _ = vf.get_feed()  # get n frames accumulation
+    vio, nonvio, _, _, _ = vf.get_feed()  # get n frames accumulation
     # insert new record
-    print("vio >> ", vio)
-    print("non vio >> ", nonvio)
     db.session.add(Violations(
         violations=vio, nonviolations=nonvio, time=datetime.now()))
     db.session.commit()
@@ -125,7 +131,7 @@ def update_violations_graph(n):
 def update_sevidx_graph(n):
     # plotting variables
     t, sevidx = [], []
-    _, _, sev = vf.get_feed()  # get n frames accumulation
+    _, _, sev, _, _ = vf.get_feed()  # get n frames accumulation
     # insert new record
     db.session.add(Sevidx(sev=sev, time=datetime.now()))
     db.session.commit()
@@ -142,16 +148,84 @@ def update_sevidx_graph(n):
     return fig2
 
 
+@app.callback(Output('heat-map', 'figure'), [Input('interval-component', 'n_intervals')])
+def update_heatmap(n):
+    _, _, _, viox, vioy = vf.get_feed()
+    plotlist = []
+    for x, y in zip(viox, vioy):
+        db.session.add(Vioxy(violocx=x, violocy=y, time=datetime.now()))
+    db.session.commit()
+    rows = Vioxy.query.all()
+    for r in rows:
+        plotlist.append((r.violocx, r.violocy))
+    heatmapper = Heatmapper(
+        point_strength=0.5, point_diameter=300, opacity=0.35)
+    heatmapres = heatmapper.heatmap_on_img(plotlist, heatmap)
+    heatmapres.save(heatmap_res)
+    vf.clear_feed()
+    img_width = 800
+    img_height = 400
+    scale_factor = 0.5
+
+    # Add invisible scatter trace.
+    # This trace is added to help the autoresize logic work.
+    # fig4.add_trace(
+    #     go.Scatter(
+    #         x=[0, img_width * scale_factor],
+    #         y=[0, img_height * scale_factor],
+    #         mode="markers",
+    #         marker_opacity=0
+    #     )
+    # )
+
+    # # Configure axes
+    # fig4.update_xaxes(
+    #     visible=False,
+    #     range=[0, img_width * scale_factor]
+    # )
+
+    # fig4.update_yaxes(
+    #     visible=False,
+    #     range=[0, img_height * scale_factor],
+    #     # the scaleanchor attribute ensures that the aspect ratio stays constant
+    #     scaleanchor="x"
+    # )
+
+    # Add image
+    fig4.add_layout_image(
+        dict(
+            x=0,
+            sizex=img_width * scale_factor,
+            y=img_height * scale_factor,
+            sizey=img_height * scale_factor,
+            xref="x",
+            yref="y",
+            opacity=1.0,
+            layer="below",
+            sizing="stretch",
+            source=heatmap_res)
+    )
+
+    # Configure other layout
+    fig4.update_layout(
+        width=img_width * scale_factor,
+        height=img_height * scale_factor,
+        margin={"l": 0, "r": 0, "t": 0, "b": 0},
+    )
+    return fig4
+
+
 if __name__ == '__main__':
     vf = ViolationsFeed()
     # init vio graph
     fig1 = go.Figure()
     fig2 = go.Figure()
-    # init violations db
-    db.session.add(Violations(violations=0, nonviolations=0,
-                              time=datetime.now()))
-    db.session.add(Sevidx(sev=0.0, time=datetime.now()))
-    db.session.commit()
+    fig3 = go.Figure()
+    fig4 = go.Figure()
+    # heatmap Static Image
+    static_heatmap = 'data/test_images/heat.png'
+    heatmap_res = 'data/test_images/heatres.png'
+    heatmap = Image.open(static_heatmap)
     # init and load yolo network
     net = YoloPeopleDetector()
     net.load_network()
--- src/data_feed/data_feeder.py
+++ src/data_feed/data_feeder.py
@@ -27,27 +27,35 @@ class ViolationsFeed(DataFeed):
 
     def __init__(self):
         super().__init__()
-        self.viofeed = []
-        self.nonviofeed = []
-        self.sevidx = []
-        self.vio = 0
-        self.nonvio = 0
-        self.sevidxavg = 0.0
+        self.viofeed = []  # list to accumulate violations per update interval
+        self.nonviofeed = []  # list to accumulate non violations per update interval
+        self.sevidx = []  # list to accumulate severity index per update interval
+        self.violocationsx = []  # list to accumulate violations coordinates per update interval
+        self.violocationsy = []
+        self.vio = 0  # final violations (sum(len(viofeed)))
+        self.nonvio = 0  # final nonviolations (sum(len(nonviolations)))
+        self.sevidxavg = 0.0  # average severity index per update interval
 
     def feed_new(self, new_feed: tuple):
         self.viofeed.append(new_feed[0])
         self.nonviofeed.append(new_feed[1])
         self.sevidx.append(new_feed[2])
+        for pair in new_feed[0]:
+            self.violocationsx.append(np.median([pair[0][0], pair[1][0]]))
+            self.violocationsy.append(np.median([pair[0][1], pair[1][1]]))
 
     def get_feed(self):
+        '''
+        get update interval accumulations
+        '''
         self.vio = sum([len(i) for i in self.viofeed])
         self.nonvio = sum([len(i) for i in self.nonviofeed])
-        print("sev list", self.sevidx)
         if len(self.sevidx) > 0:
             self.sevidxavg = np.mean(self.sevidx)
         else:
             self.sevidxavg = 0.0
-        return self.vio, self.nonvio, self.sevidxavg
+
+        return self.vio, self.nonvio, self.sevidxavg, self.violocationsx, self.violocationsy
 
     def clear_feed(self):
         self.__init__()
